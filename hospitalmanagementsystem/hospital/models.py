519e24e2c9101e6ddb699441fe5d57059313b669
Nedzhin <nurseiit.bakkali@nu.edu.kz>
2022-11-06 20:59:52 +0600

    added patient and doctors models

--- hospitalmanagementsystem/hospital/models.py
+++ hospitalmanagementsystem/hospital/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+  name = models.CharField(max_length=50)
+  email = models.EmailField(unique = True)
+  gender = models.CharField(max_length = 20)
+  phonenumber = models.CharField(max_length = 15)
+  address = models.CharField(max_length = 100)
+  birthdate = models.DateField()
+  bloodgroup = models.CharField(max_length = 5)
+
+  def __str__(self):
+    return self.name
+
+class Doctor(models.Model):
+  name = models.CharField(max_length=50)
+  email = models.EmailField(unique = True)
+  gender = models.CharField(max_length = 20)
+  phonenumber = models.CharField(max_length = 15)
+  address = models.CharField(max_length = 100)
+  birthdate = models.DateField()
+  bloodgroup = models.CharField(max_length = 5)
+  departmentID = models.CharField(max_length = 20)
+
+  def __str__(self):
+    return self.name
+
+
--- hospitalmanagementsystem/hospitalmanagementsystem/urls.py
+++ hospitalmanagementsystem/hospitalmanagementsystem/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from hospital.views import *
+from hospital.views import*
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', homepage, name='homepage'),
 ]
